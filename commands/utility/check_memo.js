8b7d73fbf4c60dda6ff0dac9c6a9c3b7b614b3c0
Anomalous Sentiment <63189795+Anomalous-Sentiment@users.noreply.github.com>
2024-10-27 03:33:31 +0800

    Working database queries using sequelize

--- commands/utility/check_memo.js
+++ commands/utility/check_memo.js
@@ -1,20 +1,14 @@
 const { SlashCommandBuilder } = require('discord.js');
 const { fetch } = require('undici');
+const Sequelize = require('sequelize');
+const DataTypes = Sequelize.DataTypes
+
+const sequelize = new Sequelize(process.env.DATABASE_URL);
+
+var Memoria = require('../../models/combined_memoria_list.js')(sequelize, DataTypes);
+console.log(process.env.DATABASE_URL)
 
 
-console.log(process.env.DB_CONNECTION_STRING)
-
-// import { Kysely, PostgresDialect } from 'kysely';
-// import { DB } from 'kysely-codegen';
-// import { Pool } from 'pg';
-
-// const db = new Kysely<DB>({
-//   dialect: new PostgresDialect({
-//     pool: new Pool({
-//       connectionString: process.env.DATABASE_URL,
-//     }),
-//   }),
-// });
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -39,13 +33,6 @@ module.exports = {
 		const image1Res = await fetch(image1.url);
 		const image1Buffer = await image1Res.arrayBuffer();
 
-		// try {
-		// 	await sequelize.authenticate();
-		// 	console.log('Connection has been established successfully.');
-		//   } catch (error) {
-		// 	console.error('Unable to connect to the database:', error);
-		// }
-
 		// Send to analyzer
 		const headers = {'Content-Type': 'image/*'}
 		const res = await fetch('https://recognizer.tooler.tokyo/api/v1/recognize', {method: 'POST', body: image1Buffer, headers: headers});
@@ -55,26 +42,16 @@ module.exports = {
 		// Parse JSON and get list of JP names
 		const jpNames = json[0]['objects'].map((element) => element['name'])
 		console.log(jpNames)
+
 		// Search DB for matching JP names
-		// const rows = await db.selectFrom('users').selectAll().execute();
-		// const memoria = await db
-		// 	.selectFrom('combined_memoria_list')
-		// 	.selectAll()
-		// 	.where('jp_name', 'in', jpNames)
-		// 	.execute()
-		// console.log(memoria)
+		const memoMatches = await Memoria.findAll({
+			attributes: { exclude: ['id'] },
+			where: {
+				jp_name: jpNames
+			}
+		})
 
-
-		var msg = ''
-		console.log(json[0]['objects'])
-		if(!json[0]['success'] || json[0]['objects'].length == 0)
-		{
-			msg = 'No objects detected'
-		}
-		else
-		{
-			msg = json[0]['objects']
-		}
+		console.log(memoMatches)
 
 		await interaction.editReply('Finished');
 	},
