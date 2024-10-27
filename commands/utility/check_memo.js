2e886dd544c7da9dd49c5d40f41b0c2f2142452c
Anomalous Sentiment <63189795+Anomalous-Sentiment@users.noreply.github.com>
2024-10-27 21:00:31 +0800

    Refactored chaeck_memo to use utility functions and also implemented some error handling and support for multiple attachments

--- commands/utility/check_memo.js
+++ commands/utility/check_memo.js
@@ -1,14 +1,8 @@
 const { SlashCommandBuilder, EmbedBuilder, underline, quote, codeBlock, hideLinkEmbed, hyperlink } = require('discord.js');
 const { fetch } = require('undici');
-const Sequelize = require('sequelize');
-const DataTypes = Sequelize.DataTypes
+const { analyseImage, dbGetMemoriaData } = require('../../helpers/util.js')
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
-
-var Memoria = require('../../models/maxed_memoria.js')(sequelize, DataTypes);
 console.log(process.env.DATABASE_URL)
-
-
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -24,40 +18,79 @@ module.exports = {
 			option.setName('image3').setDescription('Third image to analyse')
 		),
 	async execute(interaction) {
+		const t0 = performance.now();
 		await interaction.deferReply();
 		const image1 = interaction.options.getAttachment('image1')
 		const image2 = interaction.options.getAttachment('image2')		
 		const image3 = interaction.options.getAttachment('image3')
+		const imgArr = [image1, image2, image3]
+		const fetchPromiseArr = []
+		const analysisPromiseArr = []
+		let uniqueJpNamesArr = []
 
-		// Get attachments as array buffer
-		const image1Res = await fetch(image1.url);
-		const image1Buffer = await image1Res.arrayBuffer();
+		for (const img of imgArr)
+		{
+			if (img)
+			{
+				// Create promise to fetch image if image exists
+				// Returns as arrayBuffer
+				fetchPromiseArr.push(fetch(img.url)
+					.then(res => {
+						if (res.ok)
+						{
+							return res.arrayBuffer()
+						}
+						else
+						{
+							throw new Error('Failed to fetch attachment image');
+						}
+
+					})
+					.catch(err => {
+						console.log(err)
+					}
+				))
+			}
+		}
+		const arrayBufferArr = await Promise.all(fetchPromiseArr)
+
+		for (const arrayBuffer of arrayBufferArr)
+		{
+			// Create a promise to analyse image for each arrayBufferArr
+			if (arrayBuffer)
+				{
+					// Create promise to fetch image if image exists
+					// Returns as arrayBuffer
+					analysisPromiseArr.push(analyseImage('memoria', arrayBuffer))
+				}
+
+		}
 
 		// Send to analyzer
-		const headers = {'Content-Type': 'image/*'}
-		const res = await fetch('https://recognizer.tooler.tokyo/api/v1/recognize', {method: 'POST', body: image1Buffer, headers: headers});
-		const json = await res.json();
+		// const uniqueJpNamesArr = await analyseImage('memoria', image1Buffer)
 
-		// Parse JSON and get list of JP names
-		const jpNames = json[0]['objects'].map((element) => element['name'])
-		console.log(jpNames)
+		// Wait for all image to be analysed
+		const results = await Promise.all(analysisPromiseArr)
+
+		// Combine all detected JP memoria names into single array
+		for (const jpNameArray of results)
+		{
+			uniqueJpNamesArr = uniqueJpNamesArr.concat(jpNameArray)
+		}
+
+		// Filter array to remove duplicates
+		uniqueJpNamesArr = [...new Set(uniqueJpNamesArr)];
+
+
+
+		// Filter results for all unique JP memoria names
 
 		// Filter so memoria only appear once (unique_id)
 		// card_type <= 4 is VG, higher is RG. Use to filter
 		// Use window funciton, over rarity & awakened to get highest form of memo
 
-		// Search DB for matching JP names
-		const memoMatches = await Memoria.findAll({
-			// attributes: { exclude: ['id'] },
-			attributes: ['unique_id', 'en_name', 'gvg_en_name', 'gvg_en_desc', 'auto_en_desc', 'awakened', 'super_awakened'],
-			where: {
-				jp_name: jpNames
-			},
-			order: [
-				['unique_id', 'DESC'],
-			]
-		})
-
+		// Search DB for matching JP names. Use lang to decide language
+		const memoMatches = await dbGetMemoriaData(uniqueJpNamesArr)
 		// console.log(memoMatches)
 		const embedArray = []
 		const awkEmbedArray = []
@@ -80,11 +113,10 @@ module.exports = {
 		var embedMaxFields = 25
 		for (let ii = 0; ii < memoMatches.length; ii++) {
 			let memo = memoMatches[ii]
-
-			let nameStr = `${underline(memo['en_name'])}`
-			let valueStr = `${hyperlink('Wiki Link', `https://assaultlily.wiki/wiki/Last_Bullet:${memo['en_name'].split(' ').join('_')}`)}
+			let nameStr = `${underline(memo['name'])}`
+			let valueStr = `${hyperlink('Wiki Link', `https://assaultlily.wiki/wiki/Last_Bullet:${memo['name'].split(' ').join('_')}`)}
 			${hyperlink('Mini DB Link (Slow)', `https://www.mini-allbw-db.dev/card/${memo['unique_id']}`)}
-			${codeBlock(memo['gvg_en_desc'])}${codeBlock(memo['auto_en_desc'])}`
+			${codeBlock(memo['gvg_desc'])}${codeBlock(memo['auto_desc'])}`
 			let newField = {name: nameStr, value: valueStr, inline: true}
 
 			// Check if awakened/super awakened
@@ -110,9 +142,6 @@ module.exports = {
 				embedMsg.fields.push(newField)
 			}
 		  }
-
-		console.log(embedArray)
-		console.log(embedMsg)
 		if (embedMsg)
 		{
 			// Add final embedMsg to array if it exists
@@ -124,6 +153,9 @@ module.exports = {
 			// Add final awkEmbedMsg to array if it exists
 			awkEmbedArray.push(awkEmbedMsg)
 		}
+
+		const t1 = performance.now();
+		console.log(`Execution took ${t1 - t0} milliseconds.`);
 
 		if (memoMatches.length > 0)
 		{
