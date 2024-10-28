da6ff5f37e7761875957dffa205925fb73da1ef4
Anomalous Sentiment <63189795+Anomalous-Sentiment@users.noreply.github.com>
2024-10-28 18:35:49 +0800

    Refactored check_memo to use the strategy pattern in preparation of new subcommand

--- commands/utility/check_memo.js
+++ commands/utility/check_memo.js
@@ -22,6 +22,9 @@ const LANGAUGES = [
     {value: 'jp', name: 'JP'},
 ]
 
+const MAX_EMBED_FIELDS = 25
+const MAX_EMBED_SIZE = 6000
+
 const ROLE_MAP = new Map(ROLE_OPTIONS.map(({value, name}) => {
     return [value, name]
 }))
@@ -30,30 +33,54 @@ console.log(ROLE_MAP)
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('check_memo')
-		.setDescription('Detects memoria within the attched image')
-		.addAttachmentOption(option => 
-			option.setName('image1').setDescription('Image to analyse').setRequired(true)
-		)
-		.addAttachmentOption(option => 
-			option.setName('image2').setDescription('Second image to analyse')
-		)
-		.addAttachmentOption(option => 
-			option.setName('image3').setDescription('Third image to analyse')
-		)
-		.addIntegerOption(option =>
-			option.setName('role')
-				.setDescription('Used to determine which forms of awakened memoria to show (VG or RG forms)')
-				.addChoices(...ROLE_OPTIONS)
-			)
-		.addStringOption((option =>
-			option.setName('lang')
-				.setDescription('Language to display memoria data')
-				.addChoices(...LANGAUGES)
-			),),
+		.setName('check')
+		.setDescription('Commmand for checking images for memoria or orders')
+			.addSubcommand(subcommand =>
+				subcommand
+				.setName('memoria')
+				.setDescription('Detects memoria within the attched images')
+				.addAttachmentOption(option => 
+					option.setName('image1').setDescription('Image to analyse').setRequired(true)
+				)
+				.addAttachmentOption(option => 
+					option.setName('image2').setDescription('Second image to analyse')
+				)
+				.addAttachmentOption(option => 
+					option.setName('image3').setDescription('Third image to analyse')
+				)
+				.addIntegerOption(option =>
+					option.setName('role')
+						.setDescription('Used to determine which forms of awakened memoria to show (VG or RG forms)')
+						.addChoices(...ROLE_OPTIONS)
+					)
+				.addStringOption((option =>
+					option.setName('lang')
+						.setDescription('Language to display memoria data')
+						.addChoices(...LANGAUGES)
+					)))
+			.addSubcommand(subcommand => 
+				subcommand
+				.setName('orders')
+				.setDescription('Detects orders within the attched images')
+				.addAttachmentOption(option => 
+					option.setName('image1').setDescription('Image to analyse').setRequired(true)
+				)
+				.addAttachmentOption(option => 
+					option.setName('image2').setDescription('Second image to analyse')
+				)
+				.addAttachmentOption(option => 
+					option.setName('image3').setDescription('Third image to analyse')
+				)
+				.addStringOption((option =>
+					option.setName('lang')
+						.setDescription('Language to display memoria data')
+						.addChoices(...LANGAUGES)
+					))
+			),
 	async execute(interaction) {
-		const embedMaxFields = 25
-		const maxEmbedSize = 6000
+		const subcommand = interaction.options.getSubcommand()
+		console.log(subcommand)
+
 		const t0 = performance.now();
 		await interaction.deferReply();
 		const image1 = interaction.options.getAttachment('image1')
@@ -62,106 +89,194 @@ module.exports = {
 		const role = interaction.options.getInteger('role')
 		const lang = interaction.options.getString('lang')
 		const imgArr = [image1, image2, image3]
-		const fetchPromiseArr = []
-		const analysisPromiseArr = []
+
+		
+		const analyser = new MemoriaAnalyser(imgArr, lang, role, interaction)
+		await analyser.start()
+
+		const t1 = performance.now();
+		console.log(`Execution took ${t1 - t0} milliseconds.`);
+	},
+};
+
+class Analyser {
+	constructor(imgArr, lang, role, interaction)
+	{
+		this.imgArr = imgArr
+		this.lang = lang
+		this.role = role
+		this.interaction = interaction
+		this.arrayBuffers = []
+	}
+
+	async start()
+	{
+		let dbData = []
+		let jpNames = []
+
+		// Get discord attachments as binary data
+		await this.getData()
+
+		// Send the binary data through the API for analysis
+		jpNames = await this.analyseData(this.arrayBuffers)
+
+		// Using the returned data (List of JP names detected), retrieve the associated data from out database
+		dbData = await this.getDbData(jpNames)
+
+		// Format the data returned from our database and save to class variable
+		this.formatResults(dbData)
+
+		// Expects to responds using interaction object
+		await this.respond()
+	}
+
+	async getData()
+	{
+		let fetchPromiseArr = []
+		let arrayBufferArr = []
+		console.log('Getting attachment data...')
+		for (const img of this.imgArr)
+			{
+				if (img)
+				{
+					// Create promise to fetch image if image exists
+					// Returns as arrayBuffer
+					fetchPromiseArr.push(fetch(img.url)
+						.then(res => {
+							if (res.ok)
+							{
+								return res.arrayBuffer()
+							}
+							else
+							{
+								throw new Error('Failed to fetch attachment image');
+							}
+	
+						})
+						.catch(err => {
+							console.log(err)
+						}
+					))
+				}
+			}
+			arrayBufferArr = await Promise.all(fetchPromiseArr)
+		this.arrayBuffers = arrayBufferArr
+	}
+
+	async analyseData(type)
+	{
+		let analysisPromiseArr = []
 		let uniqueJpNamesArr = []
-		const embedArray = []
-		const awkEmbedArray = []
-		let embedMsgTemplate = {
+		console.log(`Analysing data of type: ${type}...`)
+		for (const arrayBuffer of this.arrayBuffers)
+			{
+				// Create a promise to analyse image for each arrayBufferArr
+				if (arrayBuffer)
+				{
+					// Create promise to fetch image if image exists
+					// Returns as arrayBuffer
+					analysisPromiseArr.push(analyseImage('memoria', arrayBuffer))
+				}
+	
+			}
+	
+			// Send to analyzer
+			// Wait for all image to be analysed
+			const results = await Promise.all(analysisPromiseArr)
+	
+			// Combine all detected JP memoria names into single array
+			for (const jpNameArray of results)
+			{
+				uniqueJpNamesArr = uniqueJpNamesArr.concat(jpNameArray)
+			}
+	
+			// Filter array to remove duplicates
+			uniqueJpNamesArr = [...new Set(uniqueJpNamesArr)];
+
+		return uniqueJpNamesArr
+	}
+
+	async getDbData()
+	{
+		console.log('Getting DB data...')
+	}
+
+	formatResults()
+	{
+		console.log('Building embed arrays...')
+	}
+
+	async respond()
+	{
+		console.log('Responding to command...')
+	}
+}
+
+class MemoriaAnalyser extends Analyser {
+	constructor(imgArr, lang, role, interaction)
+	{
+		super(imgArr, lang, role, interaction)
+		this.embedArray = []
+		this.awkEmbedArray = []
+		this.embedMsgTemplate = {
 			title: 'Normal Memoria',
 			description: 'Assumes evolved forms. Memoria are not displayed in any particular order.',
 			color: 0x0099FF,
 			fields: []
 		}
 
-		let awkEmbedMsgTemplate = {
+		this.awkEmbedMsgTemplate = {
 			title: 'Detected Possible Awakened Memoria',
 			description: 'Displays memoria that might be awakened and all the possible versions. Only one form of the memoria is actually present in the grid',
 			color: 0xFF0000,
 			fields: []
 		}
+	}
 
+	async analyseData()
+	{
+		console.log('Getting mamoria analysed...')
+		return await super.analyseData('memoria')
+	}
 
-		for (const img of imgArr)
-		{
-			if (img)
-			{
-				// Create promise to fetch image if image exists
-				// Returns as arrayBuffer
-				fetchPromiseArr.push(fetch(img.url)
-					.then(res => {
-						if (res.ok)
-						{
-							return res.arrayBuffer()
-						}
-						else
-						{
-							throw new Error('Failed to fetch attachment image');
-						}
-
-					})
-					.catch(err => {
-						console.log(err)
-					}
-				))
-			}
-		}
-		const arrayBufferArr = await Promise.all(fetchPromiseArr)
-
-		for (const arrayBuffer of arrayBufferArr)
-		{
-			// Create a promise to analyse image for each arrayBufferArr
-			if (arrayBuffer)
-				{
-					// Create promise to fetch image if image exists
-					// Returns as arrayBuffer
-					analysisPromiseArr.push(analyseImage('memoria', arrayBuffer))
-				}
-
-		}
-
-		// Send to analyzer
-		// Wait for all image to be analysed
-		const results = await Promise.all(analysisPromiseArr)
-
-		// Combine all detected JP memoria names into single array
-		for (const jpNameArray of results)
-		{
-			uniqueJpNamesArr = uniqueJpNamesArr.concat(jpNameArray)
-		}
-
-		// Filter array to remove duplicates
-		uniqueJpNamesArr = [...new Set(uniqueJpNamesArr)];
-
-		// Filter so memoria only appear once (unique_id)
+	async getDbData(uniqueJpNamesArr)
+	{
+		console.log('Retrieving memoria data from DB...')
+		let memoMatches = []
 		// card_type <= 4 is VG, higher is RG. Use to filter
-		// Use window funciton, over rarity & awakened to get highest form of memo
 
 		// Search DB for matching JP names. Use lang to decide language
-		let memoMatches = await dbGetMemoriaData(uniqueJpNamesArr, lang ? lang : 'en')
+		memoMatches = await dbGetMemoriaData(uniqueJpNamesArr, this.lang ? this.lang : 'en')
+		return memoMatches
+	}
+
+	formatResults(dbData)
+	{
+		let unformattedData = dbData
+		console.log('Formatting memoria data...')
 
 		// Check if role was specified
-		if (role)
+		if (this.role)
 		{
 			// Filter awakened & super awakened memoria according to role
 			// Define a function for checking whether RG or VG memo
 			// Initially set to check for VG
 			let roleCheckFunc = (type) => type < 5
 
-			if (role >= 5)
+			if (this.role >= 5)
 			{
 				// If role is RG, change function to check for RG memo
 				roleCheckFunc = (type) => type >= 5
 			}
 
 			// Apply filter based on role
-			memoMatches = memoMatches.filter(memo => {
+			unformattedData = dbData.filter(memo => {
 				if (memo['awakened'] && roleCheckFunc(memo['card_type']))
 				{
 					// If awakened, return the RG form of the memoria
 					return memo
 				}
-				else if (memo['super_awakened'] && (memo['card_type'] == role))
+				else if (memo['super_awakened'] && (memo['card_type'] == this.role))
 				{
 					// If super awakened, return the memoria that matches their role
 					return memo
@@ -173,18 +288,19 @@ module.exports = {
 				}
 				// Otherwise, ignore
 			})
+			console.log(unformattedData)
 
 			// change awk embed title and descriptions
-			awkEmbedMsgTemplate.title = `Awakened Memoria (${ROLE_MAP.get(role)} Forms)`
-			awkEmbedMsgTemplate.description = `Displays awakened for of memoria aligned with your selected role ${ROLE_MAP.get(role)}`
+			this.awkEmbedMsgTemplate.title = `Awakened Memoria (${ROLE_MAP.get(this.role)} Forms)`
+			this.awkEmbedMsgTemplate.description = `Displays awakened for of memoria aligned with your selected role (${ROLE_MAP.get(this.role)})`
 
 		}
 
-		let embedMsg = structuredClone(embedMsgTemplate)
-		let awkEmbedMsg = structuredClone(awkEmbedMsgTemplate)
+		let embedMsg = structuredClone(this.embedMsgTemplate)
+		let awkEmbedMsg = structuredClone(this.awkEmbedMsgTemplate)
 
-		for (let ii = 0; ii < memoMatches.length; ii++) {
-			let memo = memoMatches[ii]
+		for (let ii = 0; ii < unformattedData.length; ii++) {
+			let memo = unformattedData[ii]
 			let nameStr = `${underline(memo['name'])}`
 			let valueStr = `${hyperlink('Wiki Link', `https://assaultlily.wiki/wiki/Last_Bullet:${memo['en_name'].split(' ').join('_')}`)}
 			${hyperlink('TW DB Link', `https://allb.game-db.tw/memoria/${memo['jp_name']}`)}
@@ -195,10 +311,10 @@ module.exports = {
 			// Check if awakened/super awakened
 			if (memo['awakened'] || memo['super_awakened'])
 			{
-				if (awkEmbedMsg.fields.length != 0 && awkEmbedMsg.fields.length % embedMaxFields == 0)
+				if (awkEmbedMsg.fields.length != 0 && awkEmbedMsg.fields.length % MAX_EMBED_FIELDS == 0)
 				{
-					// Push the old embed onto array and create a new embed if limit is reached
-					awkEmbedArray.push(awkEmbedMsg)
+					// Push the old embed onto array and create a new embed if fields limit is reached
+					this.awkEmbedArray.push(awkEmbedMsg)
 					awkEmbedMsg = {
 						title: 'Detected Awakened Memoria',
 						color: 0xFF0000,
@@ -209,11 +325,11 @@ module.exports = {
 				awkEmbedMsg.fields.push(newField)
 
 				// Check if embed exceeds limit
-				if (embedLength(awkEmbedMsg) > maxEmbedSize)
+				if (embedLength(awkEmbedMsg) > MAX_EMBED_SIZE)
 				{
 					// If exceeded, pop the last fields and create new embed and add the popped field to new embed
 					let lastField = awkEmbedMsg.fields.pop()
-					awkEmbedArray.push(awkEmbedMsg)
+					this.awkEmbedArray.push(awkEmbedMsg)
 					awkEmbedMsg = {
 						title: 'Detected Awakened Memoria',
 						color: 0xFF0000,
@@ -225,9 +341,9 @@ module.exports = {
 			}
 			else
 			{
-				if (embedMsg.fields.length != 0 && embedMsg.fields.length % embedMaxFields == 0)
+				if (embedMsg.fields.length != 0 && embedMsg.fields.length % MAX_EMBED_FIELDS == 0)
 				{
-					embedArray.push(embedMsg)
+					this.embedArray.push(embedMsg)
 					embedMsg = {
 						title: 'Detected Memoria',
 						description: 'Assumes evolved forms. Memoria are not displayed in any particular order.',
@@ -235,15 +351,15 @@ module.exports = {
 						fields: []
 					}
 				}
-				// Assumes will never pass 25 elements. If it does pass, extra checks are needed like for awakened memo above
+
 				embedMsg.fields.push(newField)
 
 				// Check if embed exceeds limit
-				if (embedLength(embedMsg) > maxEmbedSize)
+				if (embedLength(embedMsg) > MAX_EMBED_SIZE)
 				{
 					// If exceeded, pop the last fields and create new embed and add the popped field to new embed
 					let lastField = embedMsg.fields.pop()
-					embedArray.push(embedMsg)
+					this.embedArray.push(embedMsg)
 					embedMsg = {
 						title: 'Detected Memoria',
 						description: 'Assumes evolved forms. Memoria are not displayed in any particular order.',
@@ -254,40 +370,42 @@ module.exports = {
 			}
 		}
 
-		if (embedMsg)
+		if (embedMsg.fields.length > 0)
 		{
 			// Add final embedMsg to array if it exists
-			embedArray.push(embedMsg)
+			this.embedArray.push(embedMsg)
 		}
 
-		if (awkEmbedMsg)
+		if (awkEmbedMsg.fields.length > 0)
 		{
 			// Add final awkEmbedMsg to array if it exists
-			awkEmbedArray.push(awkEmbedMsg)
+			this.awkEmbedArray.push(awkEmbedMsg)
 		}
+	}
 
-		const t1 = performance.now();
-		console.log(`Execution took ${t1 - t0} milliseconds.`);
-
-		if (memoMatches.length > 0)
+	async respond()
+	{
+		console.log('Responding with memoria... ')
+		if (this.awkEmbedArray.length > 0 || this.embedArray.length > 0)
 		{
 			// Send the first msg with first embed
-			await interaction.editReply({ embeds: [embedArray.shift()] });
-			for(const embed of embedArray)
+			await this.interaction.editReply({ embeds: [this.embedArray.shift()] });
+
+			for(const embed of this.embedArray)
 			{
-				await interaction.followUp({ embeds: [embed] });
+				await this.interaction.followUp({ embeds: [embed] });
 			}
 
-			for(const embed of awkEmbedArray)
+			for(const embed of this.awkEmbedArray)
 			{
-				await interaction.followUp({ embeds: [embed]})
+				await this.interaction.followUp({ embeds: [embed]})
 			}
 
 
 		}
 		else
 		{
-			await interaction.editReply('Did not detect memoria');
+			await this.interaction.editReply('Did not detect memoria');
 		}
-	},
-};
+	}
+}
