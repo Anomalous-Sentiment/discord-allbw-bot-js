7e7f41b282f872513b441ffacc0dd44096f8d037
Anomalous Sentiment <63189795+Anomalous-Sentiment@users.noreply.github.com>
2024-10-29 02:17:27 +0800

    Added more optional images that can be uploaded

--- commands/utility/check.js
+++ commands/utility/check.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, ApplicationEmojiManager } = require('discord.js');
 const { ROLE_OPTIONS, LANGAUGES } = require('../../helpers/constants.js')
 const { MemoriaAnalyser } = require('../../classes/memoria-analyser.js')
 const { OrderAnalyser } = require('../../classes/order-analyser.js')
@@ -10,7 +10,7 @@ module.exports = {
 			.addSubcommand(subcommand =>
 				subcommand
 				.setName('memoria')
-				.setDescription('Detects memoria within the attched images')
+				.setDescription('Detects memoria within the attached images')
 				.addAttachmentOption(option => 
 					option.setName('image1').setDescription('Image to analyse').setRequired(true)
 				)
@@ -19,6 +19,24 @@ module.exports = {
 				)
 				.addAttachmentOption(option => 
 					option.setName('image3').setDescription('Third image to analyse')
+				)
+				.addAttachmentOption(option => 
+					option.setName('image4').setDescription('Fourth mage to analyse')
+				)
+				.addAttachmentOption(option => 
+					option.setName('image5').setDescription('Fifth image to analyse')
+				)
+				.addAttachmentOption(option => 
+					option.setName('image6').setDescription('Sixth image to analyse')
+				)
+				.addAttachmentOption(option => 
+					option.setName('image7').setDescription('Seventh to analyse')
+				)
+				.addAttachmentOption(option => 
+					option.setName('image8').setDescription('Eighth image to analyse')
+				)
+				.addAttachmentOption(option => 
+					option.setName('image9').setDescription('Ninth image to analyse')
 				)
 				.addIntegerOption(option =>
 					option.setName('role')
@@ -43,6 +61,24 @@ module.exports = {
 				.addAttachmentOption(option => 
 					option.setName('image3').setDescription('Third image to analyse')
 				)
+				.addAttachmentOption(option => 
+					option.setName('image4').setDescription('Fourth mage to analyse')
+				)
+				.addAttachmentOption(option => 
+					option.setName('image5').setDescription('Fifth image to analyse')
+				)
+				.addAttachmentOption(option => 
+					option.setName('image6').setDescription('Sixth image to analyse')
+				)
+				.addAttachmentOption(option => 
+					option.setName('image7').setDescription('Seventh to analyse')
+				)
+				.addAttachmentOption(option => 
+					option.setName('image8').setDescription('Eighth image to analyse')
+				)
+				.addAttachmentOption(option => 
+					option.setName('image9').setDescription('Ninth image to analyse')
+				)
 				.addStringOption((option =>
 					option.setName('lang')
 						.setDescription('Language to display memoria data')
@@ -55,9 +91,15 @@ module.exports = {
 		const image1 = interaction.options.getAttachment('image1')
 		const image2 = interaction.options.getAttachment('image2')		
 		const image3 = interaction.options.getAttachment('image3')
+		const image4 = interaction.options.getAttachment('image4')
+		const image5 = interaction.options.getAttachment('image5')		
+		const image6 = interaction.options.getAttachment('image6')
+		const image7 = interaction.options.getAttachment('image7')
+		const image8 = interaction.options.getAttachment('image8')		
+		const image9 = interaction.options.getAttachment('image9')
 		const role = interaction.options.getInteger('role')
 		const lang = interaction.options.getString('lang')
-		const imgArr = [image1, image2, image3]
+		const imgArr = [image1, image2, image3, image4, image5, image6, image7, image8, image9]
 		const subcommand = interaction.options.getSubcommand()
 		const analyserMap = new Map([
 			['memoria', MemoriaAnalyser],
