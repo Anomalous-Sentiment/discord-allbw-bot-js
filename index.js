692972fcdc6a3a66c2fd481d433b420f487f8692
Anomalous Sentiment <63189795+Anomalous-Sentiment@users.noreply.github.com>
2024-10-29 14:48:49 +0800

    Added custom status

--- index.js
+++ index.js
@@ -60,10 +60,10 @@ client.on(Events.InteractionCreate, async interaction => {
 });
 
 // When the client is ready, run this code (only once).
-// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
-// It makes some properties non-nullable.
 client.once(Events.ClientReady, readyClient => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+	
+	// Set custom status
     client.user.setActivity({ type: ActivityType.Custom, emoji: '', name: 'Contemplating my usefulness' })
 
 });
